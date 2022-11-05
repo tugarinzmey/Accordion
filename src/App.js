e58e5e2e529b5cd6d56964f5a53df663b8993528
tugarinzmey <71755647+tugarinzmey@users.noreply.github.com>
2022-11-05 16:26:39 +0300

    accordion

--- src/App.js
+++ src/App.js
@@ -1,23 +1,82 @@
-import logo from './logo.svg';
+import React, { useState, createContext, useContext } from "react";
 import './App.css';
+
+const AccordionContext = createContext();
+const ItemContext = createContext();
+
+function AccordionItemHeader({children}){
+  console.log("itemHeader render");
+
+  const {onClick, selectedKey} = useContext(AccordionContext);
+  const eventKey = useContext(ItemContext);
+  let className = selectedKey === eventKey ? "header active" : "header"
+  return(
+    <>
+    <div className = {className} onClick={() => onClick(eventKey)}>
+      {children}
+      <img src="https://www.svgrepo.com/show/16711/right-arrow.svg"></img>
+    </div>
+    </>
+  );
+}
+
+function AccordionItemBody({children}){
+  console.log("itemBody render");
+
+  const {_, selectedKey} = useContext(AccordionContext);
+  const eventKey = useContext(ItemContext);
+  let className = selectedKey === eventKey ? "accordion-body active" : "accordion-body";
+  return(
+    <>
+      <div className={className}>
+        {children}
+      </div>
+    </>
+  );
+}
+
+function AccordionItem({children, eventKey}){
+  console.log("item render");
+
+  return(
+    <ItemContext.Provider value={eventKey}>
+      <div className="item">
+        {children}
+      </div>
+    </ItemContext.Provider>
+  );
+}
+
+function MarkAccordion({children}) {
+  console.log("accordion render");
+
+  const [selectedKey, setSelectedKey] = useState(0);
+  function onClick(key){
+    setSelectedKey(key);
+    key === selectedKey ? setSelectedKey(0) : setSelectedKey(key);
+  }
+  return (
+    <AccordionContext.Provider value={{onClick, selectedKey}}>
+    <div className="accordion">
+      {children}
+    </div>
+  </AccordionContext.Provider>
+  );
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="ramka">
+      <MarkAccordion>
+        <AccordionItem eventKey={1}>
+          <AccordionItemHeader>First</AccordionItemHeader>
+          <AccordionItemBody> I am first</AccordionItemBody>
+        </AccordionItem>
+        <AccordionItem eventKey={2}>
+          <AccordionItemHeader>Second</AccordionItemHeader>
+          <AccordionItemBody> I am second</AccordionItemBody>
+        </AccordionItem>
+      </MarkAccordion>
     </div>
   );
 }
